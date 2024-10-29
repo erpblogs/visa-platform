0df7263d90c80a5e64584eaecbcbf185ae9bd378
Quangtv <trinhvanquangf1@gmail.com>
2024-10-29 15:12:27 +0700

    update module name

---
+++ visa_management/__manifest__.py
@@ -0,0 +1,46 @@
+{
+    'name': 'Visa Management',
+    'version': '1.0',
+    'summary': 'Manage visa applications and processes',
+    'description': """
+        Visa Management System for managing visa applications, 
+        processing, and tracking.
+    """,
+    'category': 'Services/Visa',
+    'author': 'Your Name',
+    'website': 'https://www.yourwebsite.com',
+    'depends': [
+        'base',
+        'mail',
+        'survey',
+        'account',
+    ],
+    'data': [
+        'security/visa_security.xml',
+        'security/ir.model.access.csv',
+        'data/visa_sequence.xml',
+        'views/visa_application_views.xml',
+        'views/visa_category_views.xml',
+        'views/visa_stage_views.xml',
+        'views/visa_partner_views.xml',
+        'views/visa_survey_views.xml',
+        'reports/visa_reports.xml',
+        'reports/visa_report_templates.xml',
+        'views/visa_menu.xml',
+        'views/visa_menu_survey.xml',
+        'views/visa_menu_partner.xml',
+    ],
+    'demo': [
+        'demo/visa_demo.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'visa_management/static/src/scss/visa_styles.scss',
+            'visa_management/static/src/js/visa_widgets.js',
+        ],
+    },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'sequence': 1,
+} 
